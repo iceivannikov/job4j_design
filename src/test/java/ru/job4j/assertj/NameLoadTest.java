9df571d67a20eec9a5df7ca16384b4915325c253
Viktor Ivannikov <iceivannikov@gmail.com>
2024-04-25 14:32:24 +0300

    4. Утверждения с исключениями [#504886]

--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -1,5 +1,54 @@
+package ru.job4j.assertj;
+
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
+
 class NameLoadTest {
-  
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkLengthZero() {
+        NameLoad nameLoad = new NameLoad();
+        String[] value = {};
+        assertThatThrownBy(() -> nameLoad.parse(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void checkContainsEqualSign() {
+        NameLoad nameLoad = new NameLoad();
+        String[] value = {"key:value"};
+        assertThatThrownBy(() -> nameLoad.parse(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain the symbol '='")
+                .hasMessageContaining(value[0]);
+    }
+
+    @Test
+    void checkStartsWithEqualSign() {
+        NameLoad nameLoad = new NameLoad();
+        String[] value = {"=key=value"};
+        assertThatThrownBy(() -> nameLoad.parse(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a key")
+                .hasMessageContaining(value[0]);
+    }
+
+    @Test
+    void checkEndsWithEqualSign() {
+        NameLoad nameLoad = new NameLoad();
+        String[] value = {"keyvalue="};
+        assertThatThrownBy(() -> nameLoad.parse(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("does not contain a value")
+                .hasMessageContaining(value[0]);
+    }
 }
