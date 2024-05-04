74b58c24b4573c39f2e8dba98eaf6fd83763b70b
Viktor Ivannikov <iceivannikov@gmail.com>
2024-05-04 16:20:49 +0300

    4. Используя контейнер на базе связанного списка создать контейнер Stack [#71474 #494256]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,2 +1,25 @@
-package ru.job4j.collection;public class SimpleQueue {
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> input = new SimpleStack<>();
+    private final SimpleStack<T> output = new SimpleStack<>();
+    int size;
+
+    public T poll() {
+        if (size == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        while (size != 0) {
+            output.push(input.pop());
+            size--;
+        }
+        return output.pop();
+    }
+
+    public void push(T value) {
+        input.push(value);
+        size++;
+    }
 }
