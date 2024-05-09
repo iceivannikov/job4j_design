3dfb22b606f85a2729d7646bd04f3897da0a2cdb
Viktor Ivannikov <iceivannikov@gmail.com>
2024-05-09 19:53:16 +0300

    4. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class User {
     private final String name;
@@ -11,5 +14,27 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthday = Calendar.getInstance();
+        User user1 = new User("Viktor", 5, birthday);
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
+        int backet1 = hash1 & 15;
+        User user2 = new User("Viktor", 5, birthday);
+        int hashCode2 = user2.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
+        int backet2 = hash2 & 15;
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        Set<Map.Entry<User, Object>> entries = map.entrySet();
+        for (Map.Entry<User, Object> entry : entries) {
+            System.out.println(entry.getKey());
+        }
+        System.out.printf("user1 - hashcode: %s, hash: %s, backet: %s", hashCode1, hash1, backet1);
+        System.out.println();
+        System.out.printf("user1 - hashcode: %s, hash: %s, backet: %s", hashCode2, hash2, backet2);
     }
 }
