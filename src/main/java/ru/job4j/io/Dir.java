eeec678c5ae0143eeda2fd12f119fd3732cd2a35
Viktor Ivannikov <iceivannikov@gmail.com>
2024-06-09 20:09:53 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,20 +1,24 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/Users/viktor/IdeaProjects");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("size : %s", file.getTotalSpace()));
-        for (File subfile : file.listFiles()) {
+        System.out.printf("size : %s%n", file.getTotalSpace());
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
             if (subfile.isDirectory()) {
-                for (File listFile : subfile.listFiles()) {
+                for (File listFile : Objects.requireNonNull(subfile.listFiles())) {
                     System.out.printf("File name: %s, file size: %d%n", listFile.getName(), listFile.length());
                 }
             }
