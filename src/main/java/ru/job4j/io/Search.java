b0c94ae6797f558fb920ee9d9b2f87920350c1f5
Viktor Ivannikov <iceivannikov@gmail.com>
2024-06-10 21:11:24 +0300

    12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -8,13 +8,24 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Path.of(".");
-        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        checkArgs(args);
+        Path start = Path.of(args[0].trim());
+        String extension = args[1].trim();
+        search(start, path -> path.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    private static void checkArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("The number of elements must be two");
+        }
+        if (args[0].trim().isEmpty() || args[1].trim().isEmpty()) {
+            throw new IllegalArgumentException("One of the arguments is not specified");
+        }
     }
 }
