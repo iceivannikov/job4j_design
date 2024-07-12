a5ed86612f24506db1a8bc06adcb4173445c03e5
Viktor Ivannikov <iceivannikov@gmail.com>
2024-07-12 15:48:47 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/json/hwjson/Main.java
@@ -0,0 +1,32 @@
+package ru.job4j.serialization.json.hwjson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Product(true, 150.0, "Telephone",
+                new Manufacturer("Apple", "USA"),
+                new String[] {"electronics", "sale"});
+
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(product));
+
+        final String productJson =
+                "{"
+                        + "\"inStock\":false,"
+                        + "\"price\":200.0,"
+                        + "\"name\":TV,"
+                        + "\"manufacturer\":"
+                        + "{"
+                        + "\"name\":\"LG\","
+                        + "\"country\":\"Korea\""
+                        + "},"
+                        + "\"tags\":"
+                        + "[\"electronics\",\"sale\"]"
+                        + "}";
+
+        final Product productMod = gson.fromJson(productJson, Product.class);
+        System.out.println(productMod);
+    }
+}
---
+++ src/main/java/ru/job4j/serialization/json/hwjson/Product.java
@@ -0,0 +1,30 @@
+package ru.job4j.serialization.json.hwjson;
+
+import java.util.Arrays;
+
+public class Product {
+    private final boolean inStock;
+    private final double price;
+    private final String name;
+    private final Manufacturer manufacturer;
+    private final String[] tags;
+
+    public Product(boolean inStock, double price, String name, Manufacturer manufacturer, String[] tags) {
+        this.inStock = inStock;
+        this.price = price;
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.tags = tags;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{"
+                + "inStock=" + inStock
+                + ", price=" + price
+                + ", name='" + name + '\''
+                + ", manufacturer=" + manufacturer
+                + ", tags=" + Arrays.toString(tags)
+                + '}';
+    }
+}
